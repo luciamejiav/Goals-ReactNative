de740c778d0e71ff65e7db14d26c91840eb9c052
Lucia <luciamejia03@gmail.com>
2024-01-29 09:28:35 +0100

    GoalInput-addGoalHandler

--- App.js
+++ App.js
@@ -1,88 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { View, TextInput, Button, Text, StyleSheet, ScrollView, FlatList } from 'react-native';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
 
   //declaramos el hook de estado de componente "newGoal"
-  const [newGoal, setNewGoal] = useState("");
   const [myGoals, setMyGoals] = useState([]);
 
-  function textChangeHandler(enteredText){
-    setNewGoal(enteredText); //agregamos texto
-    console.log(newGoal);
-  }
+  function addGoalHandler(newGoalText){
+    //crea un nuevo array con las metas actuales y añade al final el newGoal, esto se llama destructuración
+    setMyGoals(myCurrentGoals => [...myCurrentGoals,
+      { //añadimos objetos
+        id: Date.now(),  //fecha actual
+        text: newGoalText,
 
-  function addGoalHandler(){
-    setMyGoals(myCurrentGoals => [...myCurrentGoals, newGoal]) //crea un nuevo array con las metas actuales y añade al final el newGoal, esto se llama destructuración
-    console.log(myGoals)
+    }]);
   }
 
   return (
     <View style = {styles.container}>
-      
-      <View style = {styles.inputcontainer}>
-        <TextInput 
-          onChangeText={textChangeHandler} 
-          style = {styles.textinput} 
-          placeholder='Input your goals'
-        />
-        <Button 
-          title='Add Goal'
-          onPress={addGoalHandler}  //manejador para añadir la meta
-        />
-      </View>
+      <GoalInput onNewGoal = {addGoalHandler}/> 
 
       <View style = {styles.goalsContainer}>
+        {/*Podemos hacer un FlatList o un ScrollView*/}
         <FlatList
           data={myGoals}
           renderItem={(dataItem) => (
-            <View style = {styles.goalItem} key={dataItem.item}>
+            <View style = {styles.goalItem} key={dataItem.item.id}>
               {/*Comentario: añadimos las metas*/}
-              <Text style = {styles.goalText}>{dataItem.item}</Text>  
+              <Text style = {styles.goalText}>{dataItem.item.text}</Text>  
             </View>
           )}
         />
       </View>
-      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = new StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 70,
     paddingHorizontal: 15
   },
-
-  inputcontainer: {
-    flex: 1, //sin esto la línea y el borde de abajo del rectangulo se juntan
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 20,
-    borderBottomWidth: 1, 
-    borderBottomColor: "#CCCCCC"
-  },
-
-  textinput: {
-    borderWidth: 1, 
+  textInput: {
+    borderColor: "#CCCCCC",
+    borderWidth: 1,
     width: "70%",
     padding: 10
   },
-
   goalsContainer: {
-    flex:5
+    flex: 5,
   },
-   
   goalItem: {
     padding: 20,
     backgroundColor: "#092FE0",
-    marginBottom: 10, 
-    borderRadius: 12
+    marginBottom: 10,
+    borderRadius: 12,
   },
-
   goalText: {
     color: "white"
   }
---
+++ components/GoalInput.js
@@ -0,0 +1,48 @@
+//rfc + tabulador
+import { useState } from 'react'
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+export default function GoalInput( {onNewGoal}) {
+    //declaramos el hook de estado de componente "newGoal"
+    const [newGoal, setNewGoal] = useState("");
+
+    function textChangeHandler(enteredText){
+        setNewGoal(enteredText); //agregamos texto
+    }
+
+    function onPressHandler() {
+        if(newGoal) {
+            onNewGoal(newGoal)
+        }
+        setNewGoal(""); //cuando introduzcamos una palabra y demos al botón se vacía la celda
+    }
+
+    return (
+        <View style = {styles.inputcontainer}>
+            <TextInput 
+                onChangeText={textChangeHandler} 
+                style = {styles.textInput} 
+                value={newGoal}
+                placeholder='Input your goals'
+            />
+            <Button 
+                title='Add Goal'
+                onPress={onPressHandler}  //manejador para añadir la meta
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    inputcontainer: {
+        flex: 1, //sin esto la línea y el borde de abajo del rectangulo se juntan
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 20,
+        borderBottomWidth: 1, 
+        borderBottomColor: "#CCCCCC"
+      }
+})
+
+
